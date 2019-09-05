9365fc48ca7b53eb6166c50ec1d33e90cd366d38
Billy Sjöberg <billy.sjoberg@gmail.com>
2019-09-05 12:52:58 +0200

    simplifications 2

--- src/main/java/samples/sample3/server/Endpoint.java
+++ src/main/java/samples/sample3/server/Endpoint.java
@@ -4,23 +4,14 @@ import util.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("/hello")
 public class Endpoint {
 
-    private static AtomicInteger concurrency = new AtomicInteger();
-
     @GET
     public String hello() throws InterruptedException {
         Logger.log("in endpoint");
-        concurrency.incrementAndGet();
         Thread.sleep(1000);
-        concurrency.decrementAndGet();
         return "hello";
-    }
-
-    public static int getConcurrency(){
-        return concurrency.get();
     }
 }
